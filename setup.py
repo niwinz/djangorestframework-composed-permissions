8481e279637037c6812a9fbe3a227d8978c84365
Harry White <harry.white424@gmail.com>
2019-02-27 09:35:27 +1100

    Clean up setup, bump version

--- setup.py
+++ setup.py
@@ -2,29 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import sys
 
 with open('README.rst') as file:
     long_description = file.read()
 
 INSTALL_REQUIRES = [
-    "django >=1.4",
+    "django >= 1.11",
     "djangorestframework",
 ]
 
 setup(
-    name = "djangorestframework-composed-permissions",
-    version = "0.2",
-    description = "Composed permissions for django-rest-framework",
-    long_description = long_description,
-    keywords = "django, rest, restframework, permissions",
-    author = "Andrey Antukh",
-    author_email = "niwi@niwi.be",
-    url = "https://github.com/niwibe/djangorestframework-composed-permissions",
-    license = "BSD",
-    packages = find_packages(),
-    install_requires = INSTALL_REQUIRES,
-    classifiers = [
+    name="djangorestframework-composed-permissions",
+    version="0.2.1",
+    description="Composed permissions for django-rest-framework",
+    long_description=long_description,
+    keywords="django, rest, restframework, permissions",
+    author="Andrey Antukh",
+    author_email="niwi@niwi.be",
+    url="https://github.com/niwibe/djangorestframework-composed-permissions",
+    license="BSD",
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Framework :: Django",
         "Intended Audience :: Developers",
@@ -32,9 +31,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Topic :: Internet :: WWW/HTTP",
     ]
 )
